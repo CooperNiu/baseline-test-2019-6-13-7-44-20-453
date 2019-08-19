e8c5409c9d083f96c0856489e74932bfb0537559
CooperNiu <niuyanno.1@outlook.com>
2019-08-19 18:42:28 +0800

    Add files via upload

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,12 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  let result = [];
+  for (let i = 0; i < collectionA.length; i++) {
+    if (collectionB.indexOf(collectionA[i]) !== -1) {
+      // 判断集合A中的元素是否在B中存在，使用求下标的方法，如果不存在的话，下标为-1
+      result.push(collectionA[i]);
+    }
+  }
+  return result;
 }
